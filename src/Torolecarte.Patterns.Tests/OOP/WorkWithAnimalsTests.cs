75ab377f836e3e86adce4389b252f34d8cdae666
Heitor Corrêa <5171086+torolecarte@users.noreply.github.com>
2022-09-25 23:46:28 -0300

    Tests: Added animal inheritance

---
+++ src/Torolecarte.Patterns.Tests/OOP/WorkWithAnimalsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torolecarte.Patterns.OOP;
+
+namespace Torolecarte.Patterns.Tests.OOP
+{
+    public class WorkWithAnimalsTests
+    {
+        [Test]
+        public void PlayWithCatAndDog()
+        {
+            // Arrange.
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Act.
+            dog.Name = "Fido";
+            Console.WriteLine($"Dog's name: {dog.Name}");
+
+            dog.DigHole();
+            //dog.Weight = -1;
+
+
+
+
+            // Assert.
+            Assert.That(true);
+        }
+    }
+}
