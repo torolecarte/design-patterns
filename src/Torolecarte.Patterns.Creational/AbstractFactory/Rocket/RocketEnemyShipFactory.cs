f807c685cb9c42fc4de79329bdd6a5735b154b16
Heitor Corrêa <5171086+torolecarte@users.noreply.github.com>
2022-09-29 16:47:34 -0300

    Code cleanup

--- src/Torolecarte.Patterns.Creational/AbstractFactory/Rocket/RocketEnemyShipFactory.cs
+++ src/Torolecarte.Patterns.Creational/AbstractFactory/Rocket/RocketEnemyShipFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Torolecarte.Patterns.Creational.AbstractFactory.Rocket
+﻿namespace Torolecarte.Patterns.Creational.AbstractFactory.Rocket
 {
     public class RocketEnemyShipFactory : IEnemyShipFactory
     {
