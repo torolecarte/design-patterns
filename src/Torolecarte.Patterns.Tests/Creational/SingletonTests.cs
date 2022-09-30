20651392936fc78395b08f9f5af7d7935d573198
Heitor Corrêa <5171086+torolecarte@users.noreply.github.com>
2022-09-29 21:05:47 -0300

    Added example of Singleton using Scrabble

---
+++ src/Torolecarte.Patterns.Tests/Creational/SingletonTests.cs
@@ -0,0 +1,26 @@
+﻿using Torolecarte.Patterns.Creational.Singleton;
+
+namespace Torolecarte.Patterns.Tests.Creational
+{
+    public class SingletonTests
+    {
+        [Test]
+        public void GetTiles_RequestThreeTiles_ReturnsRandomizedThreeTiles()
+        {
+            // Arrange.
+            int countExpected = 3;
+            var instance = ScrabbleSingleton.Instance;
+
+            // Act.
+            var result = instance.GetLetterTiles(countExpected);
+            Console.WriteLine("First result: " + String.Join(", ", result));
+
+            result = instance.GetLetterTiles(countExpected);
+            Console.WriteLine("Second result: " + String.Join(", ", result));
+
+            // Assert.
+            Assert.That(result.Count, Is.EqualTo(countExpected));
+            Assert.That(instance.AllLettersInBag.Count - (countExpected * 2), Is.EqualTo(instance.AvailableLetters.Count));
+        }
+    }
+}
